0e8820bb78e0516099497d344c89bb4476a711ed
gaixas1 <gaixas1@gmail.com>
2017-06-26 10:36:33 +0200

    dasd

--- src/clocksincro_main.cc
+++ src/clocksincro_main.cc
@@ -113,7 +113,7 @@ int main(int argc, char * argv[]) {
 
 	try {
 		rina::initialize("INFO", "");
-		if (dstName != "") {
+		if (dstName != string("")) {
 			cout << "Clock server | " << apName << ":" << apInstance <<endl;
 			clocksincro_server s(apName, apInstance);
 			s.register_ap(difs);
